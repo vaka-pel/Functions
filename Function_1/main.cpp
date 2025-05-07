492d1b3bb869b4372e72d48ccf9a2e4fb2aaea41
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-05-07 22:12:28 +0300

    Function1 DONE

--- Function_1/main.cpp
+++ Function_1/main.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int Fact(int b);
+int Fact(int n)
+{
+	if (n == 0)
+		return 1;
+	else return n * Fact(n - 1);
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	int b, a = 1;
-	cout << "¬ведите число: ";  cin >> b;
-	int f = Fact(b);
-	cout << f << " * " << a << " = " << f << endl;
-	a++;
-}
-int Fact(int a)
-{
-	int f = a * f;
-	return f;
+	int n;
+	cout << "¬ведите число: ";  cin >> n;
+	
+	cout << n << " = " << Fact(n) << endl << endl;
+	return;
+	
 }
