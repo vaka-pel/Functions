e73a56a65a62c6d542d1a5f968db77b41114c250
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-05-21 18:52:42 +0300

    ArraysF DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,14 +3,32 @@ using namespace std;
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
+
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
+
 void Sum(int arr[], const int n, int s = 0);
-void Avg(int arr[], const int n, int s = 0);
+void Sum(double arr[], const int n, double s = 0);
+void Sum(bool arr[], const int n, bool s = 0);
+void Sum(char arr[], const int n, char s = 0);
+
+void Avg(int arr[], const int n, int a = 0);
+void Avg(double arr[], const int n, double a = 0);
+void Avg(bool arr[], const int n, bool a = 0);
+void Avg(char arr[], const int n, char a = 0);
+
 void MinValueIn(int arr[], const int n, int min = 0);
+void MinValueIn(double arr[], const int n, double min = 0);
+void MinValueIn(bool arr[], const int n, bool min = 0);
+void MinValueIn(char arr[], const int n, char min = 0);
+
 void MaxValueIn(int arr[], const int n, int max = 0);
+void MaxValueIn(double arr[], const int n, double max);
+void MaxValueIn(bool arr[], const int n, bool max);
+void MaxValueIn(char arr[], const int n, char max);
 
 
 
@@ -25,7 +43,7 @@ void main()
 	Print(arr, n);
 	Sort(arr, n);
     Print(arr, n);
-	Sum(arr, n);
+   	Sum(arr, n);
 	Avg(arr, n);
     MinValueIn(arr, n);
     MaxValueIn(arr, n);
@@ -61,6 +79,7 @@ void FillRand(int arr[], const int n, int minRand, int maxRand)
 		arr[i] /= 100;
 	}
 }
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -77,6 +96,7 @@ void Print(double arr[], const int n)
 	}
 	cout << endl;
 }
+
 void Sort(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -84,6 +104,7 @@ void Sort(int arr[], const int n)
 		for (int j = i + 1; j < n; j++)
 		{
 			if (arr[j] < arr[i])
+
 			{
 				int buffer = arr[i];
 				arr[i] = arr[j];
@@ -108,6 +129,7 @@ void Sort(double arr[], const int n)
 		}
 	}
 }
+
 void Sum(int arr[], const int n, int s)
 {	
    	for (int i = 0; i < n; i++)
@@ -116,15 +138,68 @@ void Sum(int arr[], const int n, int s)
 	}
 	cout << "Сумма: " << s << endl;		
 }
-void Avg(int arr[], const int n, int s)
+void Sum(double arr[], const int n, double s)
+{	
+   	for (int i = 0; i < n; i++)
+ 	{
+		s += arr[i];
+	}
+	cout << "Сумма: " << s << endl;		
+}
+void Sum(bool arr[], const int n, bool s)
+{	
+   	for (int i = 0; i < n; i++)
+ 	{
+		s += arr[i];
+	}
+	cout << "Сумма: " << s << endl;		
+}
+void Sum(char arr[], const int n, char s)
+{	
+   	for (int i = 0; i < n; i++)
+ 	{
+		s += arr[i];
+	}
+	cout << "Сумма: " << s << endl;		
+}
+
+void Avg(int arr[], const int n, int a)
 { 
      for (int i = 0; i < n; i++)
 	 { 
-		 s  += arr[i];
+		 a  += arr[i];
      }
-     cout << "Среднее-арифметическое: " <<  s / n  << endl;
+     cout << "Среднее-арифметическое: " <<  a / n  << endl;
         
 }
+void Avg(double arr[], const int n, double a)
+{ 
+     for (int i = 0; i < n; i++)
+	 { 
+		 a  += arr[i];
+     }
+     cout << "Среднее-арифметическое: " <<  a / n  << endl;
+        
+}
+void Avg(bool arr[], const int n, bool a)
+{ 
+     for (int i = 0; i < n; i++)
+	 { 
+		 a  += arr[i];
+     }
+     cout << "Среднее-арифметическое: " <<  a / n  << endl;
+        
+}
+void Avg(char arr[], const int n, char a)
+{ 
+     for (int i = 0; i < n; i++)
+	 { 
+		 a  += arr[i];
+     }
+     cout << "Среднее-арифметическое: " <<  a / n  << endl;
+        
+}
+
 void MinValueIn(int arr[], const int n, int min)
 { 
 		min = arr[0];
@@ -135,7 +210,68 @@ void MinValueIn(int arr[], const int n, int min)
      cout << "Минимальное значение : " <<  min  << endl;
         
 }
+void MinValueIn(double arr[], const int n, double min)
+{ 
+		min = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] < min)min = arr[i];
+     }
+     cout << "Минимальное значение : " <<  min  << endl;
+        
+}
+void MinValueIn(bool arr[], const int n, bool min)
+{ 
+		min = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] < min)min = arr[i];
+     }
+     cout << "Минимальное значение : " <<  min  << endl;
+        
+}
+void MinValueIn(char arr[], const int n, char min)
+{ 
+		min = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] < min)min = arr[i];
+     }
+     cout << "Минимальное значение : " <<  min  << endl;
+        
+}
+
 void MaxValueIn(int arr[], const int n, int max)  
+{ 
+		max = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] > max)max = arr[i];
+     }
+     cout << "Максимальное значение : " <<  max  << endl;
+     
+}
+void MaxValueIn(double arr[], const int n, double max)  
+{ 
+		max = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] > max)max = arr[i];
+     }
+     cout << "Максимальное значение : " <<  max  << endl;
+     
+}
+void MaxValueIn(bool arr[], const int n, bool max)  
+{ 
+		max = arr[0];
+	 for (int i = 0; i < n; i++)
+	 { 
+		 if (arr[i] > max)max = arr[i];
+     }
+     cout << "Максимальное значение : " <<  max  << endl;
+     
+}
+void MaxValueIn(char arr[], const int n, char max)  
 { 
 		max = arr[0];
 	 for (int i = 0; i < n; i++)
